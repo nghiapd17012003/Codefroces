0ac7fdd0e4c08044669828ff769399504f0fdbab
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2020-05-07 20:07:52 +1200

    1345B

--- Program.cs
+++ Program.cs
@@ -1,86 +1,46 @@
 ﻿using System;
 
-namespace _1313A
+namespace _1345B
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int[] count = new int[a];  
-            for (int b = 0; b < a; b++)
-            {
-                var line = Console.ReadLine();
-                var data = line.Split(' ');
-                var x = int.Parse(data[0]); 
-                var y = int.Parse(data[1]); 
-                var z = int.Parse(data[2]);
-                if (x > 3 && y > 3 && z > 3)
-                {
-                    count[b] = 7;
-                }
+            int NoOfCase = int.Parse(Console.ReadLine());
+            int[] result = new int[NoOfCase];
 
-                if (x == 0 && y == 0 && z == 0)
+            for (int a = 0; a < NoOfCase; a++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                
+                while (n >= 2)
                 {
-                    count[b] = 0;
-                }
-                if (x == 1 && y == 1 && z == 1)
-                {
-                    count[b] = 3;
-                }
-                if ((x == 0 && y == 0 && z > 0) || (y== 0 && z == 0 && x > 0) || (z == 0 && x == 0 && y> 0))
-                {
-                    count[b] = 1;
-                }
-                if ((x == 0 && y == 1 && z >= 1) || (y == 0 && z == 1 && x>= 1) || (z == 0 && x == 1 && y >= 1))
-                {
-                    count[b] = 3;
-                }
-                if ((x == 0 && y > 1 && z > 1) || (y== 0 && z > 1 && x > 1) || (z == 0 && x> 1 && y> 1))
-                {
-                    count[b] = 3;
-                }
-                if ((x == 1 && y == 1 && z > 1) || (y == 1 && z== 1 && x> 1) || (z == 1 && x == 1 && y > 1))
-                {
-                    count[b] = 3;
-                }
-                if ((x == 1 && y> 1 && z > 1) || (y== 1 && z > 1 && x > 1) || (z == 1 && x > 1 && y > 1))
-                {
-                    count[b] = 4;
-                }
-                if (1 < x && x < 4 && 1 < y && y < 4 && 1 < z && z < 4)
-                {
-                    if (x >= y)
+                    int sum = 0;
+                    for (int b = 0; b < 99999999; b++)
                     {
-                        if (y >= z)
+                        sum = sum + 3 * b + 2;
+                        if (sum > n)
                         {
-                            count[b] = 3 + z;
+                            n = n - sum+ 3*b+2 ;
+                            result[a]++;
+                            break;
                         }
-                        else
+                        if (sum == n)
                         {
-                            count[b] = 3 + y;
+                            n = n - sum;
+                            result[a]++;
+                            break;
                         }
                     }
-                    if (x < y)
-                    {
-                        if (x >= z)
-                        {
-                            count[b] = 3 + z;
-                        }
-                        else
-                        {
-                            count[b] = 3 + x;
-                        }
-
-                    }
-
                 }
-            }
-            for (int j = 0; j < a; j++)
-            {
-                Console.WriteLine(count[j]);
-            }
+                
 
+            }
+            for (int c = 0; c < NoOfCase; c++)
+            {
+                Console.WriteLine(result[c]);
+            }
+            
         }
     }
 }
